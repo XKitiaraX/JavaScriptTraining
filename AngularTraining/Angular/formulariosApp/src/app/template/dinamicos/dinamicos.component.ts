05b714318dfc239b2162fc43ebd8ce4ca5b9e8e0
Paula Arena <paularodsor@gmail.com>
2021-03-04 10:08:04 +0100

    Angular Seccion 16 finalizada

--- AngularTraining/Angular/formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ AngularTraining/Angular/formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,15 +16,33 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  nuevoJuego: string= ''
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Paula',
+    favoritos: [
+      { id: 1, nombre: 'Splatoon'},
+      {id: 2, nombre: 'Zelda'}]
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
   guardar() {
-    console.log('Formulario posteado')
+    console.log('Formulario posteado');
   }
 
 }
