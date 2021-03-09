617c5f2cbc977bd28f72a6e7f93816127ebc62fb
Paula Arena <paularodsor@gmail.com>
2021-03-09 19:37:27 +0100

    Retirar Vue

--- VueTraining/vue-practice-master/spa-explain/lib.js
+++
@@ -1,9 +0,0 @@
-class Vue {
-  constructor(props) {
-    const app = document.querySelector(props.el);
-
-    for (let key in props.data) {
-      app.innerHTML = app.innerHTML.replace(`{{${key}}}`, props.data[key])
-    }
-  }
-}
