b02f794cb7d637684710ae0b9bcdd0c4743b5cbb
Paula Arena <paularodsor@gmail.com>
2021-03-29 11:34:53 +0200

    Angular seccion 22

---
+++ AngularTraining/Angular/10-graficasApp/src/app/shared/menu/menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [`
+    li {
+      cursor: pointer;
+    }
+  `]
+})
+export class MenuComponent {
+
+  menu: MenuItem[] = [
+    { ruta: 'graficas/barra', texto: 'Barras'},
+    { ruta: 'graficas/barra-doble', texto: 'Barras doble'},
+    { ruta: 'graficas/dona', texto: 'Donut'},
+    { ruta: 'graficas/dona-http', texto: 'Donut http'},
+  ]
+
+}
